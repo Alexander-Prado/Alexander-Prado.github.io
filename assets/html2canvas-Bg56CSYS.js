eb3201ee9969a971f8f73fc1e00a563930097d8e
Jesús Prado <jes_us100@hotmail.com>
2025-10-21 09:54:38 -0500

    Updates

--- assets/html2canvas-Bg56CSYS.js
+++ assets/html2canvas-Bg56CSYS.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-B-h87O8u.js";var t=e(((e,t)=>{
+import{n as e}from"./index-BFvDV65y.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
